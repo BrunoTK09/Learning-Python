bc3bfb753643f0e747651b22b597eca2e97faa93
BrunoTk09 <bruno.mammana.oe@gmail.com>
2024-09-07 12:58:40 -0300

    17, 18, 19 ,20

---
+++ BroCode/18 - for loops.py
@@ -0,0 +1,19 @@
+#   for loops = execute a block of code a fixed number of times.
+#               You can iterate over a range, string, sequence, etc.
+
+#basic syntax
+'''for x in range(1, 11, 2):
+    print(x)
+
+print("HAPPY NEW YEAR!")'''
+#==========================================
+'''credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    print(x)'''
+#==========================================
+'''for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)'''
