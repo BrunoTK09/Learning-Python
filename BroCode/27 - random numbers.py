e9f676cde37ba7767e90e8d9605d0746301ea02c
BrunoTk09 <bruno.mammana.oe@gmail.com>
2025-02-24 16:06:32 -0300

    2 lessons and 2 exercises

---
+++ BroCode/27 - random numbers.py
@@ -0,0 +1,17 @@
+import random
+
+#print(help(random))
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# number = random.randint(low, high)
+# number = random.random()
+# option = random.choice(options)
+# random.shuffle(cards)
+
+# print(number)
+# print(options)
+# print(cards)
---
+++ BroCode/28 - number guessing game.py
@@ -0,0 +1,35 @@
+# Python number guessing game
+import random
+
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print("Python Number Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+
+    guess = input("Enter your guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("This number is out of range")
+            print(f"Please select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low! Try again!")
+        elif guess > answer:
+            print("Too high! Try again!")
+        else:
+            print(f"CORRECT! The answer was {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+
+    else:
+        print("Invalid guess")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
